3b838a41672d74c1d43a3c853767624852bfe182
madelinemcdonald <mmm01@uw.edu>
2021-02-17 18:55:41 -0800

    average fixed

--- analysis.R
+++ analysis.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(maps)
+library(countrycode)
 all_data <- read.csv("data/population.csv", skip = 14)
 
 data <- all_data %>%
@@ -7,6 +9,8 @@ data <- all_data %>%
 dim(data)
 
 country_of_interest <- "ESP"
+country_name <- countrycode(country_of_interest, origin = 'iso3c', destination = 'country.name')
+  
 
 country_data <- data %>%
   filter(Country.of.asylum..ISO. == country_of_interest)
@@ -22,6 +26,16 @@ top_10_countries <- country_data %>%
   arrange(-Asylum.seekers) %>% #descending order
   select(Country.of.origin, Asylum.seekers)
 
-install.packages("maps")
-library(maps)
-shapefile <- map_data("world")
+shapefile <- map_data("world") #state for US
+
+shapefile <- shapefile %>%
+  mutate(Country.of.origin..ISO. = countrycode(region, origin = 'country.name', destination = 'iso3c')) %>%
+  left_join(country_data, by = "Country.of.orgin..ISO.")
+  
+ggplot(data = shapefile) +
+  geom_polygon(
+    mapping = aes(x = long, y = lat, group = group, fill = Asylum.seekers)
+    ) +
+  labs(title = paste("Number of People Seeking Asylum in", country_name), 
+       x = "", y = "", fill = "Num. People") +
+  theme_minimal()
